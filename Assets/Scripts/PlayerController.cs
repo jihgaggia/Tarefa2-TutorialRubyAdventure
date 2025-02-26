057e185b4c9e2069be13e3f9bbc95fa197a04f72
jihgaggia <jihgaggia@gmail.com>
2025-02-26 16:57:29 +0000

    4.Apply horizontal movement to the player character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,23 +14,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
         float horizontal = 0.0f;
-
         if (Keyboard.current.leftArrowKey.isPressed)
         {
-
             horizontal = -1.0f;
-
         }
         else if (Keyboard.current.rightArrowKey.isPressed)
         {
-
             horizontal = 1.0f;
-
         }
+
         Debug.Log(horizontal);
+
+
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        transform.position = position;
     }
 }
