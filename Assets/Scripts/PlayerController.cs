dcdad06ffc2f66d59f3a3f100e7c69f940f2a416
jihgaggia <jihgaggia@gmail.com>
2025-02-26 16:59:14 +0000

    6.Check your script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
+
 
     // Update is called once per frame
     void Update()
@@ -23,13 +26,26 @@ public class PlayerController : MonoBehaviour
         {
             horizontal = 1.0f;
         }
-
         Debug.Log(horizontal);
 
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
 
 
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
+
 }
+
