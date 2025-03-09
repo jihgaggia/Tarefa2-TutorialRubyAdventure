e7b603c554c7e3261b437db7f4244e41e7277930
jihgaggia <jihgaggia@gmail.com>
2025-03-09 14:25:15 +0000

    9.Check your scripts

--- Assets/Scripts/PlayControl2.cs
+++ Assets/Scripts/PlayControl2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
     // Variables related to player character movement
@@ -11,27 +12,22 @@ public class PlayerController : MonoBehaviour
     Vector2 move;
     public float speed = 3.0f;
 
-
     // Variables related to the health system
     public int maxHealth = 5;
-    int currentHealth;
     public int health { get { return currentHealth; } }
-
+    int currentHealth;
 
     // Variables related to temporary invincibility
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
 
-
-    // Variables related to animation
+    // Variables related to Animation
     Animator animator;
     Vector2 moveDirection = new Vector2(1, 0);
 
-
-    // Variables related to projectiles
+    // // Variables related to Projectile
     public GameObject projectilePrefab;
-
 
 
     // Start is called before the first frame update
@@ -39,15 +35,16 @@ public class PlayerController : MonoBehaviour
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
+        //Debug.Log(move);
 
 
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
@@ -55,7 +52,6 @@ public class PlayerController : MonoBehaviour
             moveDirection.Set(move.x, move.y);
             moveDirection.Normalize();
         }
-
 
         animator.SetFloat("Look X", moveDirection.x);
         animator.SetFloat("Look Y", moveDirection.y);
@@ -66,7 +62,9 @@ public class PlayerController : MonoBehaviour
         {
             damageCooldown -= Time.deltaTime;
             if (damageCooldown < 0)
+            {
                 isInvincible = false;
+            }
         }
 
 
@@ -75,10 +73,17 @@ public class PlayerController : MonoBehaviour
             Launch();
         }
 
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            FindFriend();
+        }
     }
 
 
-    // FixedUpdate has the same call rate as the physics system
+
+
+    // FixedUpdate has the same call rate as the physics system 
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -86,31 +91,45 @@ public class PlayerController : MonoBehaviour
     }
 
 
+
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
             if (isInvincible)
+            {
                 return;
-
+            }
             isInvincible = true;
             damageCooldown = timeInvincible;
             animator.SetTrigger("Hit");
         }
-
-
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
     }
+
 
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(moveDirection, 300);
-
-
         animator.SetTrigger("Launch");
     }
+
+
+    void FindFriend()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, moveDirection, 1.5f, LayerMask.GetMask("NPC"));
+        if (hit.collider != null)
+        {
+            NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+            if (character != null)
+            {
+                UIHandler.instance.DisplayDialogue();
+            }
+        }
+    }
+
 
 }
